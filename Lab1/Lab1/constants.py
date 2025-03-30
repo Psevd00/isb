bf532796a0167b077ca5febe06bcd313747c5ff4
Psevd00 <163391548+Psevd00@users.noreply.github.com>
2025-03-30 18:09:25 +0400

    Update constants.py

--- Lab1/Lab1/constants.py
+++ Lab1/Lab1/constants.py
@@ -1,10 +1,6 @@
-# constants.py
-
-# Алфавит для шифрования
 ALPHABET = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ '
-
-# Пути к файлам
-TASK_DIRECTORY = 'Task1'
-INPUT_FILE = 'original_text.txt'
 ENCRYPTED_FILE = 'encrypted_text.txt'
+INPUT_FILE = 'original_text.txt'
 KEY_FILE = 'key.json'
+TASK_DIRECTORY = 'Task1'
+
