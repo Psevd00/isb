9d2f5a12d4ab4d6a041ae3ec98fd2fc4c8bc2c62
Psevd00 <163391548+Psevd00@users.noreply.github.com>
2025-03-30 14:34:32 +0400

    Add files via upload

---
+++ Lab1/Task2/constants2.py
@@ -0,0 +1,6 @@
+# Пути к файлам
+CIPHER_TEXT_PATH = 'cod4.txt'
+RUSSIAN_FREQUENCIES_PATH = 'russian_letter_frequency.json'
+CIPHER_FREQUENCIES_OUTPUT = 'cipher_frequencies.json'
+MAPPING_OUTPUT = 'mapping.json'
+DECRYPTED_TEXT_OUTPUT = 'cod4_decrypted.txt'
