8ac020b422860f247af707b4515c9d4403f168a1
Psevd00 <163391548+Psevd00@users.noreply.github.com>
2025-03-30 18:11:37 +0400

    Update constants2.py

--- Lab1/Task2/constants2.py
+++ Lab1/Task2/constants2.py
@@ -1,6 +1,5 @@
-# Пути к файлам
-CIPHER_TEXT_PATH = 'cod4.txt'
-RUSSIAN_FREQUENCIES_PATH = 'russian_letter_frequency.json'
 CIPHER_FREQUENCIES_OUTPUT = 'cipher_frequencies.json'
-MAPPING_OUTPUT = 'mapping.json'
+CIPHER_TEXT_PATH = 'cod4.txt'
 DECRYPTED_TEXT_OUTPUT = 'cod4_decrypted.txt'
+MAPPING_OUTPUT = 'mapping.json'
+RUSSIAN_FREQUENCIES_PATH = 'russian_letter_frequency.json'
