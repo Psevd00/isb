50b2ef8e861892483e575fdef905c76599febdc1
Psevd00 <163391548+Psevd00@users.noreply.github.com>
2025-04-18 00:04:11 +0400

    Update constants2.py

--- Lab1/Task2/constants2.py
+++ Lab1/Task2/constants2.py
@@ -1,5 +1,10 @@
-CIPHER_FREQUENCIES_OUTPUT = 'cipher_frequencies.json'
-CIPHER_TEXT_PATH = 'cod4.txt'
-DECRYPTED_TEXT_OUTPUT = 'cod4_decrypted.txt'
-MAPPING_OUTPUT = 'mapping.json'
-RUSSIAN_FREQUENCIES_PATH = 'russian_letter_frequency.json'
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+CIPHER_FREQUENCIES_OUTPUT = os.path.join(BASE_DIR, "cipher_frequencies.json")
+CIPHER_TEXT_PATH = os.path.join(BASE_DIR, "cod4.txt")
+DECRYPTED_TEXT_OUTPUT = os.path.join(BASE_DIR, "cod4_decrypted.txt")
+KEY_PATH = os.path.join(BASE_DIR, "Key.json")
+RUSSIAN_FREQUENCIES_PATH = os.path.join(BASE_DIR, "russian_letter_frequency.json")
+
