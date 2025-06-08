3ed74c1c367c1c353c1b3b853eed81a9395c69f5
Psevd00 <163391548+Psevd00@users.noreply.github.com>
2025-06-08 20:48:20 +0400

    Update Operations.py

--- Task/Operations.py
+++ Task/Operations.py
@@ -1,35 +1,28 @@
 import json
 
 
-def write_json(filename: str, data: dict) -> None:
-    """
-    Запись данных в JSON файл
-    :param data: данные для записи
-    """
-    try:
-        with open(filename, "w", encoding="utf-8") as file:
-            json.dump(data, file, indent=4, ensure_ascii=False)
-    except PermissionError:
-        print(f"No permission to write to file {filename}.")
-    except Exception as exc:
-        print(f"Error writing JSON: {exc}")
+class JsonOperations:
+    @staticmethod
+    def write(filename: str, data: dict) -> None:
+        try:
+            with open(filename, "w", encoding="utf-8") as file:
+                json.dump(data, file, indent=4, ensure_ascii=False)
+        except PermissionError:
+            print(f"No permission to write to file {filename}.")
+        except Exception as exc:
+            print(f"Error writing JSON: {exc}")
 
-def read_json(filename: str) -> dict:
-    """
-    Чтение JSON файла
-    :return: Словарь.
-    """
-    try:
-        with open(filename, "r", encoding="utf-8") as file:
-            return json.load(file)
-    except FileNotFoundError:
-        print(f"File {filename} not found.")
-        return {}
-    except json.JSONDecodeError as e:
-        print(f"JSON format error in file {filename}: {e}")
-        return {}
-    except Exception as exc:
-        print(f"Error reading JSON: {exc}")
-        return {}
-
-
+    @staticmethod
+    def read(filename: str) -> dict:
+        try:
+            with open(filename, "r", encoding="utf-8") as file:
+                return json.load(file)
+        except FileNotFoundError:
+            print(f"File {filename} not found.")
+            return {}
+        except json.JSONDecodeError as e:
+            print(f"JSON format error in file {filename}: {e}")
+            return {}
+        except Exception as exc:
+            print(f"Error reading JSON: {exc}")
+            return {}
